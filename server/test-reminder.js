25854646a6dbabca80025b72fa1bee7289e1b76b
Anant <anantkdewangan2004@gmail.com>
2026-07-15 18:47:15 +0530

    Phase 7: reminder engine with email notifications

--- server/test-reminder.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config({ path: '.env' });
-const mongoose = require('mongoose');
-const runJob = require('./src/jobs/reminderJob');
-
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log("Connected. Testing cron init...");
-  runJob();
-  setTimeout(() => process.exit(0), 1000);
-});
